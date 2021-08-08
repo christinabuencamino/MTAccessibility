d570eac97806deb0f1b95712827faf71dcac7db7
Christina Buencamino <c.buencamino123@gmail.com>
2021-08-08 18:21:28 -0400

    Fixing some table issues; Made table dynamic

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -22,16 +22,16 @@ window.onclick = function (event) {
 
 // https://stackoverflow.com/questions/30671620/how-to-change-dropdowns-values-based-on-a-selection-of-another-dropdown-using-jq/30671798
 //Change the contents of Station dropdown depending on previous selection
-$('#NYC').on('change', function() {
+$('#NYC').on('change', function () {
     console.log($('#NYC').val());
     $('#Station').html('');
     if ($('#NYC').val() == "Brooklyn") {
-        $('#Station').append('<option value="Station NULL">Select Station...</option><option value="Station 1">Station 1: Brooklyn</option>');   
+        $('#Station').append('<option value="Station NULL">Select Station...</option><option value="Station 1">Station 1: Brooklyn</option>');
     } else if ($('#NYC').val() == "Queens") {
         $('#Station').append('<option value="Station NULL">Select Station...</option><option value="Station 2">Station 2: Queens</option>');
     } else if ($('#NYC').val() == "Manhattan") {
         $('#Station').append('<option value="Station NULL">Select Station...</option><option value="Station 3">Station 3: Manhattan</option>');
-    }  else if ($('#NYC').val() == "Bronx") {
+    } else if ($('#NYC').val() == "Bronx") {
         $('#Station').append('<option value="Station NULL">Select Station...</option><option value="Station 4">Station 4: Bronx</option>');
     } else if ($('#NYC').val() == "Staten Island") {
         $('#Station').append('<option value="Station NULL">Select Station...</option><option value="Station 5">Station 5: Staten Island</option>');
@@ -42,114 +42,126 @@ $('#NYC').on('change', function() {
 
 /* Show/Hide input box if station is/isn't selected */
 // https://stackoverflow.com/questions/15566999/how-to-show-form-input-fields-based-on-select-value?noredirect=1&lq=1
-$('#Station').on('change',function(){
-    console.log($("Station").val)
-    if( $(this).val()!= "Station NULL"){
-    $("#userInput").show()
+$('#Station').on('change', async function () {
+    console.log($("#Station").val())
+    if ($(this).val() != "Station NULL") {
+        $("#userInput").show()
     }
-    else{
-    $("#userInput").hide()
+    else {
+        $("#userInput").hide()
     }
+
+    let resp = await fetch("/api/GetComments?station=" + $("#Station").val());
+    let data = await resp.json();
+    console.log(data);
+
+    var table = document.createElement("table");
+
+    for (var userInput of data) {
+        var row = document.createElement("tr");
+        console.log(row);
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+
+        td1.innerHTML = userInput.station;
+        td2.innerHTML = userInput.comment;
+
+        row.appendChild(td1);
+        row.appendChild(td2);
+
+        table.appendChild(row); }
+
+    var websiteTable = document.getElementById("websiteTable");
+    websiteTable.innerHTML = ""; 
+    websiteTable.appendChild(table);   
 });
 
 /* Add user input to table that shows Station Name and User Input (not yet connected to Cosmos, does not save input) */
 //https://stackoverflow.com/questions/19995927/adding-html-input-to-table 
 
-document.getElementById("add").onclick = function() {
-    "use strict";
-
+document.getElementById("add").onclick = function () {
     var table = document.getElementById("table");
-    
+
     var row = document.createElement("tr");
     console.log(row);
     var td1 = document.createElement("td");
     var td2 = document.createElement("td");
 
     td1.innerHTML = $('#Station').val();
-    td2.innerHTML  = document.getElementById("input").value;
+    td2.innerHTML = document.getElementById("input").value;
 
     row.appendChild(td1);
     row.appendChild(td2);
 
-    table.children[0].appendChild(row);
-  }
-
-/* Storing user input in Cosmos DB */
-function storeInput() {
-    const config = {
-        endpoint: process.env.ENDPOINT,
-        key: process.env.KEY,
-        databaseId: "mtaccessibility project",
-        containerId: "user inputs",
-        partitionKey: {kind: "Hash", paths: ["/inputs"]}
-      };
+    table.appendChild(row);
 }
 
 
 
+/* List of SI stations */
 const listOfStatenIslandStations = [
     {
-        station_name:'St. George'
+        station_name: 'St. George'
     },
     {
-        station_name:'Tompkinsville'
+        station_name: 'Tompkinsville'
     },
     {
-        station_name:'Stapleton'
+        station_name: 'Stapleton'
     },
     {
-        station_name:'Clifton'
+        station_name: 'Clifton'
     },
     {
-        station_name:'Grasmere'
+        station_name: 'Grasmere'
     },
     {
-        station_name:'Old Town'
+        station_name: 'Old Town'
     },
     {
-        station_name:'Dongan Hills'
+        station_name: 'Dongan Hills'
     },
     {
-        station_name:'Jefferson Avenue'
+        station_name: 'Jefferson Avenue'
     },
     {
-        station_name:'Grant City'
+        station_name: 'Grant City'
     },
     {
-        station_name:'New Dorp'
+        station_name: 'New Dorp'
     },
     {
-        station_name:'Oakwood Heights'
+        station_name: 'Oakwood Heights'
     },
     {
-        station_name:'Bay Terrace'
+        station_name: 'Bay Terrace'
     },
     {
-        station_name:'Great Kills'
+        station_name: 'Great Kills'
     },
     {
-        station_name:'Eltingville'
+        station_name: 'Eltingville'
     },
     {
-        station_name:'Annadale'
+        station_name: 'Annadale'
     },
     {
-        station_name:'Huguenot'
+        station_name: 'Huguenot'
     },
     {
-        station_name:"Prince's Bay"
+        station_name: "Prince's Bay"
     },
     {
-        station_name:'Pleasant Plains'
+        station_name: 'Pleasant Plains'
     },
     {
-        station_name:'Richmond Valley'
+        station_name: 'Richmond Valley'
     },
     {
-        station_name:'Arthur Kill'
+        station_name: 'Arthur Kill'
     },
     {
-        station_name:'Tottenville'
+        station_name: 'Tottenville'
     },
 ]
 
