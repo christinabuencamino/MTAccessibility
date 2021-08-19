d0338a81a149b2a26edc91077cac6695d176a914
Christina Buencamino <c.buencamino123@gmail.com>
2021-08-19 19:35:42 -0400

    Trying to sort user comments

--- api/GetComments/index.js
+++ api/GetComments/index.js
@@ -1,9 +1,10 @@
 module.exports = async function (context, req) {
     const comments = context.bindings.inputComments;
+    const sortedComments = comments.sort();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: comments
+        body: sortedComments
     };
 }
 
